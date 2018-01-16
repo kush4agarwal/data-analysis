cbb2b955c9e498252dc79a07fb7f6c76605e0d67
agarwk <agarwk@uw.edu>
2018-01-16 13:01:22 -0800

    A setup file to track the packages to be installed for Data Analysis

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+	name='data-analysis',
+	version='0.0.1',
+	url='www.github.com/kush4agarwal/data-analysis',
+	license='BSD',	
+	author='Kush Agarwal',
+	packages=find_packages(),
+	install_requires=['PyQt5', 'pandas', 'numpy', 'sqlalchemy', 'nltk', 'python-twitter', 'jupyter'],
+	entry_points={},
+	extras_require={'dev': ['flake8',]}, 
+)
